8d3e3e31a58742b7844a52bdb93d629213682322
sagoe1712 <sagoe.olayinka@gmail.com>
2018-09-30 18:07:01 +0100

    initial

---
+++ js/custom.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+    alert("JS works");
+    var readURL = function(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('.profile-pic').attr('src', e.target.result);
+            }
+    
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    
+
+    $(".file-upload").on('change', function(){
+        readURL(this);
+    });
+    
+    $(".upload-button").on('click', function() {
+       $(".file-upload").click();
+    });
+});
+
+$('#btn-login').click(function(){
+	alert("Login Button has been hit");
+});
